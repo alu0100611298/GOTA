03c9950d4eae50e9826bc5d32ee68043512d373a
alu0100611298 <alu0100611298@ull.edu.es>
2015-02-13 13:55:25 +0000

    Carga de imagenes segun fecha

---
+++ js/date.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	var time = new Date();
+	var dia = time.getDate();
+	var mes = time.getMonth() + 1;
+	var año = time.getFullYear();
+	var hora = time.getHours();
+	for (i = hora; i < 25; i++) { 
+	    //$("#date").append("<li><a href='galerias.html' rel='external'>" + dia + "/" + mes + "/" + año + "/" + i + "</a></li>");
+	    $("#date").append("<img src='imagenes/" + dia + "/" + mes + "/" + año + "/" + i + ".jpg' alt=''>");
+	    //<img src="imagenes/13/2/2015/1.jpg" alt="">
+	}
+
+});
