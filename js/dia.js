9414604e443042e8392c181f4ca7b83560f2bbfa
alu0100611298 <alu0100611298@ull.edu.es>
2015-02-19 21:04:34 +0000

    First pages commit

---
+++ js/dia.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	var time = new Date();
+	var dia = time.getDate();
+	var mes = time.getMonth() + 1;
+	var año = time.getFullYear();
+	var hora = time.getHours();
+	for (i = hora; i < 24; i++) {
+		$("#hoy").append("<li class='dia'><a href='#hora'><img src='cloud.dark.rain.png'><h1>" + i + ":00</h1><h3>11º</h3><p>18 km/h</p><p>0.1 mm/h</p></a></li>"); 
+		//$("#hoy").append("<li class='dia'><a href=''><img src='cloud.dark.rain.png'  class='ui-li-icon' ><h1>" + i + ":00</h1><p>Lluvia</p></a></li>"); 
+	    //$("#hoy").append("<li>" + dia + "/" + mes + "/" + año + "/" + i + "</li>");
+	    //$("#date").append("<img src='imagenes/" + dia + "/" + mes + "/" + año + "/" + i + ".jpg' alt=''>");
+	    //<img src="imagenes/13/2/2015/1.jpg" alt="">
+	}
+
+	dia = dia + 1;
+	for (i = 0; i < 13; i++) {
+		$("#mañana").append("<li class='dia'><a href='#hora'><img src='cloud.dark.rain.png'><h1>" + i + ":00</h1><h3>11º</h3><p>18 km/h</p><p>0.1 mm/h</p></a></li>"); 
+	}
+	$('#hoy').listview('refresh');
+	$('#mañana').listview('refresh');
+});
---
+++ js/jquery.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	addMap();
+});
+
+function addMap(){
+	var time = new Date();
+	var dia = addZero(time.getDate() - 1);
+	var mes = addZero(time.getMonth() + 1);
+	var año = time.getFullYear();
+	var hora = time.getHours();
+	//hora = 0;
+	$("#points").attr("value", hora);
+	$("#points").attr("min", hora);
+	hora = addZero(hora);
+	$("#hora").text("Fecha: " + dia + "/" + mes + "/" + año + " Hora: " + hora + ":00");
+	$("#image").attr("src", "http://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/" + año + "" + mes + "" + dia + "" + hora + "00_s93g.gif");
+	$("#point").change(function(){
+		var valor = $("#points").val();
+		var day = dia;
+		if (valor > 23) {
+	        valor = valor - 24;
+	        day = dia + 1;
+	    }
+		valor = addZero(valor);
+		$("#hora").text("Fecha: " + dia + "/" + mes + "/" + año + " Hora: " + valor + ":00");
+	    $("#image").attr("src", "http://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/" + año + "" + mes + "" + day + "" + valor + "00_s93g.gif");
+	});
+}
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
