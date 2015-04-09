72a1e27fb81da574348e711a4c7cc1ed9595385e
alu0100611298 <alu0100611298@ull.edu.es>
2015-04-09 20:34:31 +0100

    Galeria funciona con el inclemento de la variable

--- js/jquery.js
+++ js/jquery.js
@@ -78,45 +78,30 @@ $(document).ready(function(){
         //alert("http://10.209.2.98/media/demonio/"+select_native_1+"/zona"+meteograma+"/"+parametrizaciones+"/"+variables+"/"+valor+".png");
           //$("#image").attr("src", "http://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/" + año + "" + mes + "" + day + "" + valor + "00_s93g.gif");
       });
+      $("#addMap").click(function(){
+        var valor = $("#points").val();
+        var select_native_1 = $('#select-native-1').val();
+        var meteograma = $('#select-native-2').val();
+        var parametrizaciones = $('#select-native-3').val();
+        var variables = $('#select-native-4').val();
+        
+        //alert(data[select_native_1][0]["fotos_url"]);
+        valor = addZero(valor);
+        var imagen = "http://10.209.2.98/media/demonio/"+data[select_native_1][0]["fotos_url"]+"zona"+meteograma+"/"+parametrizaciones+"/"+variables+"/"+valor+".png";
+        $("#url").text(imagen);
+        $("#hora").text("Hora: "+valor+":00");
+        $("#image").attr("src", imagen);
+        //Recojo el indice de la variable
+        var index = data[select_native_1][0]["variables"].indexOf(variables);
+        //Uso el indice para conocer el incremento de la variable
+        var step = data[select_native_1][0]["inc_variables"][index];
+        //Cambio el atributo del input radio por el salto correspondiente a la variable
+        $("#points").attr("step", step);
+      });
   });
 });
 
-function addMap(){/*
-  var time = new Date();
-  var dia = addZero(time.getDate() - 1);
-  var mes = addZero(time.getMonth() + 1);
-  var año = time.getFullYear();
-  var hora = time.getHours();
 
-  var select_native_1 = $('#select-native-1').val();
-  var porcion = select_native_1.substring(10); 
-  //alert(porcion);
-  var meteograma = $('#select-native-2').val();
-  var parametrizaciones = $('#select-native-3').val();
-  var variables = $('#select-native-4').val();
-  
-  //hora = 0;
-  $("#points").attr("value", hora);
-  $("#points").attr("min", hora);
-  hora = addZero(hora);
-  $("#hora").text("Fecha: " + dia + "/" + mes + "/" + año + " Hora: " + hora + ":00");
-  //alert("http://10.209.2.98/media/demonio/imagenes/"+año+""+mes+""+dia+"/zona"+meteograma+"/"+parametrizaciones+"/"+variables+"/"+hora+".png");
-  $("#image").attr("src", "http://10.209.2.98/media/demonio/imagenes/"+año+""+mes+""+dia+"/zona"+meteograma+"/"+parametrizaciones+"/"+variables+"/"+hora+".png");
-  //$("#image").attr("src", "http://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/" + año + "" + mes + "" + dia + "" + hora + "00_s93g.gif");
-  $("#point").change(function(){
-    var valor = $("#points").val();
-    //dia = porcion;
-    var day = dia;
-    if (valor > 23) {
-          valor = valor - 24;
-          day = dia + 1;
-      }
-    valor = addZero(valor);
-    $("#hora").text("Fecha: " + dia + "/" + mes + "/" + año + " Hora: " + valor + ":00");
-      $("#image").attr("src", "http://10.209.2.98/media/demonio/imagenes/"+año+""+mes+""+dia+"/zona"+meteograma+"/"+parametrizaciones+"/"+variables+"/"+valor+".png");
-      //$("#image").attr("src", "http://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/" + año + "" + mes + "" + day + "" + valor + "00_s93g.gif");
-  });*/
-}
 function addZero(i) {
     if (i < 10) {
         i = "0" + i;
